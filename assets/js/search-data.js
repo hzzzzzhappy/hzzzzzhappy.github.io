25a4fed40b95c2ad8cee3e96588315c3581919de
hzzzzzhappy <161625561+hzzzzzhappy@users.noreply.github.com>
2026-05-10 16:05:26 +0000

    Deploying to gh-pages from @ hzzzzzhappy/hzzzzzhappy.github.io@d9577b3d78e25224f2c2e19ef50c7e36357ed678 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-szu-group",
+          title: "SZU Group",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/join/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
